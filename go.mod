be510d6a1d1375453b5343f308971a3957c35125
陈超“ git config --global user.email 1797249167@qq.com ssh -T git@github.com$$ ssh -T git@github.com$ git config --global user.name 陈超“ <1797249167@qq.com>
2022-05-07 22:41:17 +0800

    chore:初始化MySQL和Redis

--- go.mod
+++ go.mod
@@ -3,9 +3,9 @@ module douyin-app
 go 1.16
 
 require (
-	github.com/gin-gonic/gin v1.7.7 // indirect
+	github.com/gin-gonic/gin v1.7.7
 	github.com/go-playground/validator/v10 v10.11.0 // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/go-redis/redis/v8 v8.11.5
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
@@ -13,5 +13,7 @@ require (
 	golang.org/x/crypto v0.0.0-20220507011949-2cf3adece122 // indirect
 	golang.org/x/sys v0.0.0-20220503163025-988cb79eb6c6 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0
+	gorm.io/driver/mysql v1.3.3
+	gorm.io/gorm v1.23.5
 )
--- repository/init.go
+++ repository/init.go
@@ -1,1 +1,56 @@
 package repository
+
+import (
+	"context"
+	"douyin-app/conf"
+	"github.com/go-redis/redis/v8"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
+)
+
+func InitDB() {
+	InitMySQL()
+	InitRedis()
+}
+
+var CTX = context.Background()
+var RedisDB = redis.NewClient(&redis.Options{
+	Addr:     conf.Config.Redis.Addr,
+	Password: conf.Config.Redis.Password, // no password set
+	DB:       conf.Config.Redis.DB,       // use default DB
+})
+
+//Redis相关全局变量
+func InitRedis() {
+	_, err := RedisDB.Ping(CTX).Result()
+	if err != nil {
+		panic(err)
+	}
+}
+
+//mysql,gorm配置
+var DB *gorm.DB
+
+func InitMySQL() {
+
+	dsn := conf.Config.MYSQL.Username + ":" +
+		conf.Config.MYSQL.Password + "@tcp(" +
+		conf.Config.MYSQL.Addr + ")/" +
+		conf.Config.MYSQL.Database + "?charset=utf8mb4&parseTime=True&loc=Local"
+
+	var err error
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{}) //这里用短变量声明会有歧义
+
+	if err != nil {
+		panic(err)
+	}
+
+	//绑定结构体
+	err = DB.AutoMigrate(
+		UserInfo{},
+	)
+	if err != nil {
+		log.Panicln(err)
+	}
+}
