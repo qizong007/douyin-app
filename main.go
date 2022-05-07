83f67dc43f67f053e9933475eba40d59d6314362
陈超“ git config --global user.email 1797249167@qq.com ssh -T git@github.com$$ ssh -T git@github.com$ git config --global user.name 陈超“ <1797249167@qq.com>
2022-05-07 22:42:35 +0800

    feat:在main中初始化MySQL和Redis,启动配置端口

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"douyin-app/conf"
 	"douyin-app/handler"
+	"douyin-app/repository"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	repository.InitDB()
+
 	r := gin.Default()
 	handler.Register(r)
-	r.Run() // listen and serve on 0.0.0.0:8080
+	r.Run(conf.Config.Server.Port) // listen and serve on 0.0.0.0:8080
 }
