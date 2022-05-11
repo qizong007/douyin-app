6228cbe0a1ac9a0ad7ed5bd647ba521761f99ba3
陈超“ git config --global user.email 1797249167@qq.com ssh -T git@github.com$$ ssh -T git@github.com$ git config --global user.name 陈超“ <1797249167@qq.com>
2022-05-11 14:32:02 +0800

    feat: 增加userInfo的数据库操作函数

--- repository/user_info.go
+++ repository/user_info.go
@@ -1,11 +1,47 @@
 package repository
 
+import (
+	"douyin-app/util"
+	"gorm.io/gorm"
+	"time"
+)
+
 type UserInfo struct {
-	Id int
-	//ServiceId int
-	Name       string
+	ID         int    //逻辑id,自增
+	UserId     int64  `json:"user_id"` //业务id
+	Username   string `json:"username"`
 	Password   string
 	CreateTime int64
 	ModifyTime int64
 	DeleteTime int64
+}
+
+//在创建前设置时间
+func (user *UserInfo) BeforeCreate(*gorm.DB) error {
+	user.CreateTime = time.Now().Unix()
+	return nil
+}
+
+//判断用户名在数据库中是否存在,存在则返回true
+func ExistUserByName(username string) bool {
+	var user UserInfo
+	DB.Select("id").Where("username = ?", username).First(&user)
+	if user.ID > 0 {
+		return true
+	}
+	return false
+}
+
+func CreateUserInfo(username string, password string) (ID int, userId int64, err error) {
+	user := UserInfo{
+		UserId:   util.GenerateId(),
+		Username: username,
+		Password: password,
+	}
+
+	err = DB.Create(&user).Error
+	if err != nil {
+		return 0, 0, err
+	}
+	return user.ID, user.UserId, nil
 }
