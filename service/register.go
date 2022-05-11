43189b69317bc73b8e8c97988b28d7de1845b7a6
陈超“ git config --global user.email 1797249167@qq.com ssh -T git@github.com$$ ssh -T git@github.com$ git config --global user.name 陈超“ <1797249167@qq.com>
2022-05-11 14:29:55 +0800

    feat: 实现注册的业务

---
+++ service/register.go
@@ -0,0 +1,50 @@
+package service
+
+import (
+	"douyin-app/repository"
+	"douyin-app/util"
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func Register(c *gin.Context, username string, password string) {
+	//判断用户名是否在使用
+	if exist := repository.ExistUserByName(username); exist {
+		resp := util.HttpResponse{
+			StatusCode: util.ParamError,
+			StatusMsg:  "Username is already in use",
+			ReturnVal: map[string]interface{}{
+				"user_id": 0,
+				"token":   "",
+			},
+		}
+		util.MakeResponse(c, &resp)
+		return
+	}
+	hashPassword, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+
+	//创建用户实例,存入注册信息
+	ID, userId, err := repository.CreateUserInfo(username, string(hashPassword))
+	if err != nil {
+		resp := util.HttpResponse{
+			StatusCode: util.InternalServerError,
+			ReturnVal: map[string]interface{}{
+				"user_id": 0,
+				"token":   "",
+			},
+		}
+		util.MakeResponse(c, &resp)
+		return
+	}
+	//生成token
+	token, _ := util.GenerateToken(ID, userId)
+	resp := util.HttpResponse{
+		StatusCode: util.Success,
+		ReturnVal: map[string]interface{}{
+			"user_id": userId,
+			"token":   token,
+		},
+	}
+	util.MakeResponse(c, &resp)
+	return
+}
