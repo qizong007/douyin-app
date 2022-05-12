76ac79f690d30f524f6fd665e1bf743328b6e04f
陈超“ git config --global user.email 1797249167@qq.com ssh -T git@github.com$$ ssh -T git@github.com$ git config --global user.name 陈超“ <1797249167@qq.com>
2022-05-12 10:17:59 +0800

    refactor: 集成user_handler

--- handler/user_handler.go
+++ handler/user_handler.go
@@ -8,6 +8,33 @@ import (
 	"strings"
 )
 
+type req struct {
+	Username string `validate:"required,max=32"`
+	Password string `validate:"required,min=6,max=32"`
+}
+
+func RegisterHandler(c *gin.Context) {
+	var Req req
+	Req.Username = c.Query("username")
+	Req.Password = c.Query("password")
+	//去首尾空格
+	Req.Username = strings.TrimSpace(Req.Username)
+
+	validate := validator.New() // 创建验证器
+	err := validate.Struct(Req) // 执行验证
+	if err != nil {
+		util.MakeResponse(c, &util.HttpResponse{
+			StatusCode: util.ParamError,
+			ReturnVal: map[string]interface{}{
+				"user_id": 0,
+				"token":   "",
+			},
+		})
+		return
+	}
+	service.Register(c, Req.Username, Req.Password)
+}
+
 func LoginHandler(c *gin.Context) {
 	var Req req
 	Req.Username = c.Query("username")
