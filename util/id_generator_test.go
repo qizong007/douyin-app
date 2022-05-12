ec94b10d3840062cf032a23e8e57e0394780463e
陈超“ git config --global user.email 1797249167@qq.com ssh -T git@github.com$$ ssh -T git@github.com$ git config --global user.name 陈超“ <1797249167@qq.com>
2022-05-12 09:45:57 +0800

    refactor: 修改测试的一些小问题

--- util/id_generator_test.go
+++ util/id_generator_test.go
@@ -5,6 +5,11 @@ import (
 	"testing"
 )
 
+const (
+	maxSafeInt = 1<<53 - 1
+	minSafeInt = -1<<53 + 1
+)
+
 func TestGenerateId(t *testing.T) {
 	InitIdGenerator()
 	id1 := GenerateId()
@@ -18,6 +23,7 @@ func TestGenerateId(t *testing.T) {
 		//检验递增性
 		assert.Equal(t, true, id1 < id2)
 		//检验是否越界
-		assert.Equal(t, true, id1 < (1<<53-1) && id1 > (-1<<53+1))
+		assert.Equal(t, true, id1 <= maxSafeInt && id1 >= minSafeInt)
+		assert.Equal(t, true, id2 <= maxSafeInt && id2 >= minSafeInt)
 	}
 }
--- util/jwt_test.go
+++ util/jwt_test.go
@@ -11,11 +11,10 @@ func TestJWT(t *testing.T) {
 	InitJWTVal()
 	for i := 0; i < 10000; i++ {
 		token, err := GenerateToken(1, 1<<50)
-		assert.Equal(t, err, nil)
-
+		assert.Equal(t, nil, err)
 		c, err := ParseToken(token)
-		assert.Equal(t, err, nil)
+		assert.Equal(t, nil, err)
 		assert.Equal(t, c.UserId, int64(1<<50))
+		assert.Equal(t, c.ID, 1)
 	}
-
 }
