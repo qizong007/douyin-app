f9d70ceca0ebc9a8bf4dafda65422e3653ab08b3
陈超“ git config --global user.email 1797249167@qq.com ssh -T git@github.com$$ ssh -T git@github.com$ git config --global user.name 陈超“ <1797249167@qq.com>
2022-05-31 22:55:04 +0800

    fix: 修复init bug

--- repository/init_db.go
+++ repository/init_db.go
@@ -63,6 +63,7 @@ func initRepository() {
 	userRepo = &UserRepository{}
 	videoRepo = &VideoRepository{}
 	followRepo = &FollowRepository{}
+	commentRepo = &CommentRepository{}
 }
 
 func GetUserRepository() IUserRepository {
