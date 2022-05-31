93f26878246e9a7b9d22911382b9a4d52e12b9a1
陈超“ git config --global user.email 1797249167@qq.com ssh -T git@github.com$$ ssh -T git@github.com$ git config --global user.name 陈超“ <1797249167@qq.com>
2022-05-31 19:33:58 +0800

    feat: 完成comment repository

--- repository/init_db.go
+++ repository/init_db.go
@@ -51,13 +51,15 @@ func initMySQL() {
 }
 
 var (
-	userRepo  IUserRepository
-	videoRepo IVideoRepository
+	userRepo    IUserRepository
+	videoRepo   IVideoRepository
+	commentRepo ICommentRepository
 )
 
 func initRepository() {
 	userRepo = &UserRepository{}
 	videoRepo = &VideoRepository{}
+	commentRepo = &CommentRepository{}
 }
 
 func GetUserRepository() IUserRepository {
@@ -66,4 +68,8 @@ func GetUserRepository() IUserRepository {
 
 func GetVideoRepository() IVideoRepository {
 	return videoRepo
+}
+
+func GetCommentRepository() ICommentRepository {
+	return commentRepo
 }
