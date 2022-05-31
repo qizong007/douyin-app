fc7cc65239e76c047016648965279ba318c7a01f
陈超“ git config --global user.email 1797249167@qq.com ssh -T git@github.com$$ ssh -T git@github.com$ git config --global user.name 陈超“ <1797249167@qq.com>
2022-05-31 19:36:03 +0800

    feat(domain): 完成comment初步功能

---
+++ domain/comment.go
@@ -0,0 +1,60 @@
+package domain
+
+import (
+	"context"
+	"douyin-app/repository"
+	"log"
+	"time"
+)
+
+type Comment struct {
+	Id         int64   `json:"id"`
+	User       *Author `json:"user"`
+	Content    string  `json:"content"`
+	CreateDate string  `json:"create_date"` //评论发布日期，格式 mm-dd
+}
+
+func FillComment(comment *repository.Comment, user *repository.User) *Comment {
+	timeStr := time.Unix(comment.CreateTime, 0).Format("2006-01-02 15:04:05")
+
+	return &Comment{
+		Id: comment.CommentId,
+		User: &Author{
+			Id:            user.UserId,
+			Name:          user.Username,
+			FollowCount:   user.FollowCount,
+			FollowerCount: user.FollowerCount,
+			IsFollow:      false, //这里作者是自己,所以是false
+		},
+		Content:    comment.Content,
+		CreateDate: timeStr[5:10], //mm-dd
+	}
+}
+
+func FillCommentList(ctx context.Context, comments []*repository.Comment) ([]*Comment, error) {
+	userIds := GetUserIdsFromCommentList(comments)
+	authors, err := GetAuthorsFromIds(ctx, userIds)
+	log.Println("ids", userIds)
+	log.Println("authors", authors)
+	if err != nil {
+		return nil, err
+	}
+	commentDOs := make([]*Comment, len(comments))
+	for i := range comments {
+		commentDOs[i] = &Comment{
+			Id:         comments[i].CommentId,
+			User:       authors[i],
+			Content:    comments[i].Content,
+			CreateDate: time.Unix(comments[i].CreateTime, 0).Format("2006-01-02 15:04:05")[5:10], //mm-dd
+		}
+	}
+	return commentDOs, nil
+}
+
+func GetUserIdsFromCommentList(comments []*repository.Comment) []int64 {
+	ids := make([]int64, len(comments))
+	for i := range comments {
+		ids[i] = comments[i].UserId
+	}
+	return ids
+}
