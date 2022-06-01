eb64e9d04d1c3e7a85175bb41c77ed2c7fff3d82
陈超“ git config --global user.email 1797249167@qq.com ssh -T git@github.com$$ ssh -T git@github.com$ git config --global user.name 陈超“ <1797249167@qq.com>
2022-06-01 13:56:36 +0800

    feat:增加错误码

--- util/error.go
+++ util/error.go
@@ -11,31 +11,32 @@ const (
 	UserExisted
 	UserNotExist
 	WrongPassword
-	VideoNotExist
-	CommentNotExist
 
+	VideoNotExist
+
+	CommentNotExist
+	CommentTooLong
 	SensitiveComment
+
 	IsFollow
 	NotFollow
+
 	InternalServerError
 	ParamError
 )
 
 var ErrCode2Msg = map[int]string{
-	Success: "success",
-
-	WrongAuth: "the token is expired or invalid",
-	NoAuth:    "token not received",
-
-	UserExisted:   "user already exists",
-	UserNotExist:  "username is not exist",
-	WrongPassword: "password is wrong",
-
-	IsFollow:        "该关注记录已存在",
-	NotFollow:       "该关注记录尚未存在",
-	VideoNotExist:   "video is not exist",
-	CommentNotExist: "commentId is not exist",
-
+	Success:             "success",
+	WrongAuth:           "the token is expired or invalid",
+	NoAuth:              "token not received",
+	UserExisted:         "user already exists",
+	UserNotExist:        "username is not exist",
+	WrongPassword:       "password is wrong",
+	IsFollow:            "该关注记录已存在",
+	NotFollow:           "该关注记录尚未存在",
+	VideoNotExist:       "video is not exist",
+	CommentNotExist:     "commentId is not exist",
+	CommentTooLong:      "comment is too long",
 	SensitiveComment:    "Comment contains sensitive words",
 	InternalServerError: "internal server error",
 	ParamError:          "something wrong with param...",
@@ -49,5 +50,4 @@ var (
 	ErrIsFollow         = errors.New(ErrCode2Msg[IsFollow])
 	ErrNotFollow        = errors.New(ErrCode2Msg[NotFollow])
 	ErrSensitiveComment = errors.New(ErrCode2Msg[SensitiveComment])
-	ErrCommentNotExist  = errors.New(ErrCode2Msg[CommentNotExist])
 )
