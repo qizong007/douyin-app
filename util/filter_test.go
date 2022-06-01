c495afa9e289918d97a0ca908a9cf37df8dd6327
陈超“ git config --global user.email 1797249167@qq.com ssh -T git@github.com$$ ssh -T git@github.com$ git config --global user.name 陈超“ <1797249167@qq.com>
2022-06-01 15:37:50 +0800

    feat:增加sensitive_filter功能

---
+++ util/filter_test.go
@@ -0,0 +1,28 @@
+package util
+
+import (
+	"github.com/go-playground/assert/v2"
+	"log"
+	"testing"
+)
+
+func TestFilter(t *testing.T) {
+	err := InitFilter()
+	if err != nil {
+		log.Println(err)
+	}
+	ok, w := Filter.FindIn("你是不是傻逼")
+	assert.Equal(t, true, ok)
+	assert.Equal(t, "傻逼", w)
+}
+
+func BenchmarkFilter(b *testing.B) {
+	err := InitFilter()
+	if err != nil {
+		log.Println(err)
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, _ = Filter.FindIn("你是不是傻逼")
+	}
+}
