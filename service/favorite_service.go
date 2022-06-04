4612ef7c1a5e7a415477c0cea4a2f8e2fd97da26
shmilyu <39026016+codelyc@users.noreply.github.com>
2022-06-04 11:04:25 +0800

    Update favorite_service.go

--- service/favorite_service.go
+++ service/favorite_service.go
@@ -23,7 +23,7 @@ func FavoriteAction(c *gin.Context, userId int64, videoId int64, actionType stri
 	case unLikeAction:
 		return Unlike(c, userId, videoId)
 	}
-	return nil
+	return ParamError
 }
 
 //Like
