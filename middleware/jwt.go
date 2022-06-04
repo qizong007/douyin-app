f98086ec2cb547c866007bdc17aee5eb3a05680a
陈超“ git config --global user.email 1797249167@qq.com ssh -T git@github.com$$ ssh -T git@github.com$ git config --global user.name 陈超“ <1797249167@qq.com>
2022-06-04 12:09:19 +0800

    feat： 增加JWT中间件

---
+++ middleware/jwt.go
@@ -0,0 +1,35 @@
+package middleware
+
+import (
+	"douyin-app/util"
+	"errors"
+	"github.com/gin-gonic/gin"
+	"log"
+)
+
+func JWT(c *gin.Context) {
+
+	token := c.Query("token")
+	//解析token
+	userId, err := util.ParseToken(token)
+	if err != nil { //ParseToken只会返回两种错误
+		if errors.Is(err, util.ErrNoAuth) {
+			log.Println("JWTMiddleWare Token <Nil>")
+			util.MakeResponse(c, &util.HttpResponse{
+				StatusCode: util.NoAuth,
+			})
+			c.Abort()
+			return
+		}
+		if errors.Is(err, util.ErrWrongAuth) {
+			log.Println("JWTMiddleWare Token Wrong ,Err=", err)
+			util.MakeResponse(c, &util.HttpResponse{
+				StatusCode: util.WrongAuth,
+			})
+			c.Abort()
+			return
+		}
+	}
+	c.Set("userId", userId)
+	c.Next()
+}
